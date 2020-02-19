4a76bd183d46d80b364bd9324a998d9ee3179061
George Stoica <gstoica27@gmail.com>
2020-02-18 22:51:02 -0500

    added print statement

--- rnn/model_search.py
+++ rnn/model_search.py
@@ -68,10 +68,13 @@ class RNNModelSearch(RNNModel):
       return self._arch_parameters
 
     def _loss(self, hidden, input, target):
+      print('Extracting log probabilities...')
       log_prob, hidden_next = self(input, hidden, return_h=False)
       #print('log_prob len: {} | elem shape: {}'.format(log_prob.shape, log_prob[0].shape))
       #print('target shape: {}'.format(target.shape))
+      print('computing loss...')
       loss = nn.functional.nll_loss(log_prob, target)
+      print('returning loss')
       return loss, hidden_next
 
     def genotype(self):
